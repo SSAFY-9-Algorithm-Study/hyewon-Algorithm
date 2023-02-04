e85dee6a39a6175b0e69f1c4a1f5e8ab77eb2e5a
hyewon0309 <hyewonb0309@gmail.com>
2023-02-04 11:46:44 +0900

    solve problem 11659

--- src/basic/p11659.java
+++ src/basic/p11659.java
@@ -5,26 +5,26 @@ import java.io.*;
 public class p11659 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String[] nm = br.readLine().split(" ");
-		
+
 		int n = Integer.parseInt(nm[0]);
 		int m = Integer.parseInt(nm[1]);
-		
-		int[] nums = new int[n];
+
 		String[] s_nums = br.readLine().split(" ");
-		
+
+		int[] sums = new int[n + 1];
+		sums[0] = 0;
+
+		for (int i = 1; i <= n; i++) {
+			sums[i] = sums[i - 1] + Integer.parseInt(s_nums[i-1]);
+		}
+
 		for (int test_case = 0; test_case < m; test_case++) {
 			String[] ij = br.readLine().split(" ");
-			int sum = 0;
-			
-			for (int i = Integer.parseInt(ij[0]); i <= Integer.parseInt(ij[1]); i++) {
-				sum += Integer.parseInt(s_nums[i - 1]);
-			}
-			
-			System.out.println(sum);
+			System.out.println(sums[Integer.parseInt(ij[1])] - sums[Integer.parseInt(ij[0]) - 1]);
 		}
-		
+
 		br.close();
 	}
 }
